82c1b00cfcf94ddae0bd99530fe65937d05cb5f6
Momen Noor <momen.noor@gmail.com>
2022-03-11 16:54:03 +0400

    1

--- app/DataTables/Matter2DataTable.php
+++ app/DataTables/Matter2DataTable.php
@@ -2,7 +2,11 @@
 
 namespace App\DataTables;
 
+use App\Models\Expert;
 use App\Models\Matter;
+use App\Models\Party;
+use App\Services\NumberFormatterService;
+use Carbon\Carbon;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
 use Yajra\DataTables\Html\Editor\Editor;
@@ -21,9 +25,87 @@ class Matter2DataTable extends DataTable
     {
         return datatables()
             ->eloquent($query)
+            ->editColumn('number', function ($model) {
+                return $model->year . '/' . $model->number;
+            })
+            ->editColumn('expert_id', function ($model) {
+                return '<div class="position-relative">
+                                ' . $model->expert->name . '
+                                ' . (($model->expert->name != $model->assistants->first()->name) ?
+                    '<div class="fs-7 text-muted fw-bolder">' . $model->assistants->first()->name . '</div>' : '') . '
+                        </div>';
+            })
+            ->editColumn('court_id', function ($model) {
+                return '<div class="position-relative">
+                                ' . $model->court->name . '
+                            <div class="fs-7 text-muted fw-bolder">' . $model->type->name . '</div>
+                        </div>';
+            })
+            ->editColumn('plaintiff_name', function ($model) {
+                return '<div class="position-relative">
+                                ' . \Str::of($model->plaintiffs->first()->name)->limit(30) . '
+                            <div class="text-danger">' . \Str::of($model->defendants->first()->name)->limit(30)  . '</div>
+                        </div>';
+            })
+            ->editColumn('next_session_date', function ($model) {
+                return '<div class="position-relative">
+                                ' .  $model->nextSessionDateProcedure->last()->datetime->format('Y-m-d') . '
+                            <div class="fs-7 text-muted fw-bolder">' .  $model->receivedDateProcedure->first()->datetime->format('Y-m-d') . '</div>
+                        </div>';
+            })
+            ->editColumn('claims_sum_amount', function ($model) {
+                return app(NumberFormatterService::class)->getFormattedNumber($model->claims_sum_amount);
+            })
+            ->rawColumns(['expert_id', 'court_id', 'plaintiff_name', 'next_session_date'])
             ->addColumn('action', function ($model) {
                 return view('common.table-action')->with('model', $model);
             })
+            ->filterColumn('number', function ($query, $keyword) {
+                /* $statusKey = collect(Lang::get('defination.status'))->each(function ($value, $key) use ($keyword) {
+                    return (\Str::of($value)->contains($keyword)) ? $value : $keyword;
+                })->first(); */
+                return $query->orWhere('matters.number', 'like', '%' . $keyword . '%')
+                    ->orWhere('matters.year', 'like', '%' . $keyword . '%');
+            })
+            ->filterColumn('expert_id', function ($query, $keyword) {
+                return $query->whereHas('expert', function ($q) use ($keyword) {
+                    $q->where('experts.name', 'like', '%' . $keyword . '%');
+                });
+                $matters = Matter::whereHasMorph('assistants', [Expert::class], function ($subquery, $type) use ($keyword) {
+                    if ($type === Expert::class) {
+                        $subquery->where('experts.name', 'like', '%' . $keyword . '%');
+                    }
+                })->get()->pluck('id')->toArray();
+                $query->whereIn($matters);
+            })
+            ->filterColumn('court_id', function ($query, $keyword) {
+                return $query->whereHas('court', function ($q) use ($keyword) {
+                    $q->where('courts.name', 'like', '%' . $keyword . '%');
+                })->whereHas('type', function ($q) use ($keyword) {
+                    $q->where('types.name', 'like', '%' . $keyword . '%');
+                });
+            })
+            ->filterColumn('plaintiff_name', function ($query, $keyword) {
+                $matters = Matter::whereHasMorph('plaintiffs', [Party::class], function ($subquery, $type) use ($keyword) {
+                    if ($type === Party::class) {
+                        $subquery->where('parties.name', 'like', '%' . $keyword . '%');
+                    }
+                })->get()->pluck('id')->toArray();
+                $query->whereIn($matters);
+                $matters = Matter::whereHasMorph('defendants', [Party::class], function ($subquery, $type) use ($keyword) {
+                    if ($type === Party::class) {
+                        $subquery->where('parties.name', 'like', '%' . $keyword . '%');
+                    }
+                })->get()->pluck('id')->toArray();
+                $query->whereIn($matters);
+            })
+            ->filterColumn('next_session_date', function ($query, $keyword) {
+                return $query->orWhere('procedures_received_date.datetime', 'like', '%' . $keyword . '%')
+                    ->orWhere('procedures_next_session_date.datetime', 'like', '%' . $keyword . '%');
+            })
+            ->filterColumn('claims_sum_amount', function ($query, $keyword) {
+                return $query->orWhere('claims_sum_amount', 'like', '%' . $keyword . '%');
+            });
     }
 
     /**
@@ -34,7 +116,16 @@ class Matter2DataTable extends DataTable
      */
     public function query(Matter $model)
     {
-        return $model->with('')->newQuery();
+        return $model->with([
+            'court:id,name',
+            'expert:id,name',
+            'type:id,name',
+            'assistants:id,experts.name',
+            'plaintiffs:id,parties.name',
+            'defendants:id,parties.name',
+            'receivedDateProcedure:id,procedures.matter_id,procedures.datetime',
+            'nextSessionDateProcedure:id,procedures.matter_id,procedures.datetime',
+        ])->withSum('claims', 'amount')->newQuery();
     }
 
     /**
