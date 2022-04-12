51de59e8082e80a2776982eb3584dacd5da3f152
Momen Noor <momen.noor@gmail.com>
2022-04-12 08:35:58 +0400

    fix menu

--- app/Http/Middleware/MainMenu.php
+++ app/Http/Middleware/MainMenu.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Lavary\Menu\Menu;
 
 class MainMenu
 {
@@ -16,14 +17,14 @@ class MainMenu
      */
     public function handle(Request $request, Closure $next)
     {
-        \Menu::make('mainMenu', function ($menu) {
+        (new Menu())->make('mainMenu', function ($menu) {
             $menu->add('<span class="menu-icon">
                 <i class="bi bi-bar-chart fs-3"></i>
                 </span>
                 <span class="menu-title">' . __('app.matter') . '</span>
                 <span class="menu-arrow"></span>', 'matter')->nickname('matter')->link->href('#')->attr(['class' => 'menu-link py-3']);
-            $menu->matter->add('list',['route'=>'matter.index'])->link->attr(['class' => 'menu-link py-3']);
-            $menu->matter->add('create',['route'=>'matter.create'])->link->attr(['class' => 'menu-link py-3']);
+            $menu->matter->add('list', ['route' => 'matter.index'])->link->attr(['class' => 'menu-link py-3']);
+            $menu->matter->add('create', ['route' => 'matter.create'])->link->attr(['class' => 'menu-link py-3']);
             $menu->add('<span class="menu-icon">
                 <i class="bi bi-bar-chart fs-3"></i>
                 </span>
