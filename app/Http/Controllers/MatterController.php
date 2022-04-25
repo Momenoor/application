27ff4612a87c20d91bdb33a219104b1e8c740bc9
Momen Noor <momen.noor@gmail.com>
2022-04-25 11:34:14 +0400

    fix view matter permissions

--- app/Http/Controllers/MatterController.php
+++ app/Http/Controllers/MatterController.php
@@ -50,7 +50,7 @@ class MatterController extends Controller
     {
         abort_unless(auth()->user()->canAny(['matter-view', 'matter-only-own-view']), '403');
 
-        if (auth()->user()->can('matter-only-own-view') && auth()->user()->cannot('matter-view') && ($matter->assistants->contains('id', auth()->user()->expert->id) or $matter->expert_id != auth()->user()->expert->id)) {
+        if (auth()->user()->can('matter-only-own-view') && auth()->user()->cannot('matter-view') && (!$matter->assistants->contains('id', auth()->user()->expert->id) && $matter->expert_id != auth()->user()->expert->id)) {
             abort('403');
         }
 
