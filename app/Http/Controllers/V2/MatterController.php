f342b79e8c9a5b2ab8f9363ecc864e1845b2e85a
Momen Noor <momen.noor@gmail.com>
2023-11-07 01:17:14 +0400

    create matter v2

---
+++ app/Http/Controllers/V2/MatterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\V2;
+
+use App\Http\Controllers\Controller;
+use App\Models\Court;
+use App\Models\Expert;
+use App\Models\Type;
+
+class MatterController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+        $courts = Court::all();
+        $types = Type::all();
+        $levels = collect(config('system.level'))->map(function($item){
+            return (object) $item;
+        });
+        $experts = Expert::all();
+        return view('pages.matters.v2.create', compact('courts', 'types', 'levels', 'experts'));
+    }
+}
