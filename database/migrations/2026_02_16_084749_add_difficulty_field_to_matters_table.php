8d53186a3791763dc561dadad05a994e737d38df
Momen Noor <momen.noor@gmail.com>
2026-02-16 13:40:43 +0400

    Add Difficulty Level to Matter

---
+++ database/migrations/2026_02_16_084749_add_difficulty_field_to_matters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('matters', function (Blueprint $table) {
+            $table->string('difficulty')->comment('simple,medium,exceptional')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('matters', function (Blueprint $table) {
+            $table->dropColumn('difficulty');
+        });
+    }
+};
